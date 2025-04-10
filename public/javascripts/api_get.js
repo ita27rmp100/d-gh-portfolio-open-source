47d7156d697a903bf0ac780841e58cf96a9df194
Abderahim <ita27rmp100@gmail.com>
2025-04-10 21:28:52 +0100

    New update : file management & content form % solve some issues

--- public/javascripts/api_get.js
+++ public/javascripts/api_get.js
@@ -1,11 +1,15 @@
-let username = document.title.slice(document.title.indexOf("|")+2)
+let username = document.title.slice(document.title.indexOf("|")+2) || "ita27rmp100"
 // GET users's repos {name:link} and programming skills array
 let projectsChilds = ''
 $.get(`https://api.github.com/users/${username}/repos`,function(data){
-    let NumOfRepo = Object.keys(data).length
-    for(let i=0;i<NumOfRepo;i++){
+    // let NumOfRepo = Object.keys(data).length
+    data = data.sort((a,b)=>new Date(b.created_at) - new Date(a.created_at))
+    for(let i=0;i<10;i++){
         projectsChilds += `<new-repo reponame="${data[i].name}" lnk="${data[i].html_url}" lang="${String(data[i].language).toLowerCase()}" class="p-2"></new-repo>`
     }
+    projectsChilds+=`<section class="mt-3 w-100 d-flex justify-content-center">
+                        <a href="https://github.com/${username}?tab=repositories" class="btn btn-outline-light fw-bold"><i class="fas fa-ellipsis-h"> More</i></a>
+                    </section>`
     $("#projects").html(projectsChilds)
 })
 // GET user information
@@ -17,32 +21,32 @@ $.get(`https://api.github.com/users/${username}`,function(data){
     $("#icon").attr("href",data.avatar_url)
     $("#bio").text(data.bio)
 })
-    // my github token : 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
+    // new github token : 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
 // GET the social accounts
 let socialsHTML = '' ;
 $.ajax({
     url: "https://api.github.com/graphql",
     type: "POST",
     headers: {
-        "Authorization": "Bearer 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",  // Replace with your token
+        "Authorization": "Bearer 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",  // Replace with your token
         "Content-Type": "application/json"
     },
     data: JSON.stringify({
         query: `
         {
-          user(login: "${username}") {
+            user(login: "${username}") {
+            email
             socialAccounts(first: 6) {
-              nodes {
+                nodes {
                 provider
                 url
-              }
+                }
             }
-          }
+            }
         }`
     }),
     success: function (data) {
         let JSON_res = data.data.user.socialAccounts.nodes;
-        console.log(JSON_res)
         let length = JSON_res.length
         for(i=0;i<length;i++){
             socialsHTML += `<social-acc name="${JSON_res[i].provider}" lnk="${JSON_res[i].url}"></social-acc>`
