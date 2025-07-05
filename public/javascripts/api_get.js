74ac0f329a53d22c82ad1a65dac2d56ef7fc9334
Abderahim <ita27rmp100@gmail.com>
2025-07-05 16:36:46 +0100

    done with filter bar feature

--- public/javascripts/api_get.js
+++ public/javascripts/api_get.js
@@ -2,15 +2,10 @@ let username = document.title.slice(document.title.indexOf("|")+2) || "ita27rmp1
 let api = window.vars.api
 // GET users's repos {name:link} and programming skills array
 let projectsChilds = ''
+let projectData
 $.get(`https://api.github.com/users/${username}/repos`,function(data){
-    data = data.sort((a,b)=>new Date(b.created_at) - new Date(a.created_at))
-    for(let i=0;i<5;i++){
-        projectsChilds += `<new-repo reponame="${data[i].name}" lnk="${data[i].html_url}" lang="${String(data[i].language).toLowerCase()}" class="p-2"></new-repo>`
-    }
-    projectsChilds+=`<section class="mt-3 w-100 d-flex justify-content-center">
-                        <a href="https://github.com/${username}?tab=repositories" class="btn btn-outline-light fw-bold"><i class="fas fa-ellipsis-h"> More</i></a>
-                    </section>`
-    $("#projects").html(projectsChilds)
+    projectData = data
+    filter("name")
 })
 // GET user information
 let user = {}
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,4 +10,35 @@ function chSelectedActive(id){
 }
 function filter(id){
     chSelectedActive(id)
+    switch (id) {
+        case "name":
+            projectData.sort((a,b)=>a.name.localeCompare(b.name))
+            break;
+        case "new" :
+            projectData.sort(
+                (a,b)=>new Date(b.created_at) - new Date(a.created_at)
+            )
+            break;
+        case "stars" :
+            projectData.sort(
+                (a,b)=>b.stargazers_count - a.stargazers_count
+            )
+            break;
+        default:
+            console.log("Nothing has been changed")
+            break;
+    }
+    let projectsChilds = ''
+    for(let i=0;i<5;i++){
+        projectsChilds+=`<new-repo 
+                            reponame="${projectData[i].name}" 
+                            lnk="${projectData[i].html_url}" 
+                            lang="${String(projectData[i].language).toLowerCase()}" 
+                            class="p-2">
+                        </new-repo>`
+    }
+    projectsChilds+=`<section class="mt-3 w-100 d-flex justify-content-center">
+                        <a href="https://github.com/${username}?tab=repositories" class="btn btn-outline-dark fw-bold"><i class="fas fa-ellipsis-h"> More</i></a>
+                    </section>`
+    $("#projects").html(projectsChilds)
 }
